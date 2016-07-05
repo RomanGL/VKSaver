942c18f7bb4b224d5cc3fe10fd1ea5dc1b5b3361
Roman Gladkikh <roman.gladkikh@outlook.com>
2016-07-05 15:49:23 +0600

    Повышена версия до 1.7.6.0

--- VKSaver2/VKSaver/VKSaver.WindowsPhone/Properties/AssemblyInfo.cs
+++ VKSaver2/VKSaver/VKSaver.WindowsPhone/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ВКачай 2")]
-[assembly: AssemblyCopyright("Roman Gladkikh ©  2015")]
+[assembly: AssemblyCopyright("Roman Gladkikh ©  2016")]
 [assembly: AssemblyTrademark("ВКачай")]
 [assembly: AssemblyCulture("")]
 
@@ -25,8 +25,8 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // с помощью символа '*', как показано ниже:
 // [сборка: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.4.0")]
-[assembly: AssemblyFileVersion("1.7.4.0")]
+[assembly: AssemblyVersion("1.7.6.0")]
+[assembly: AssemblyFileVersion("1.7.6.0")]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("")]
 
