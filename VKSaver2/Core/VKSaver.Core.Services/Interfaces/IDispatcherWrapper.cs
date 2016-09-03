6fba7ce0f29a79b7b7844ec3033660594aa8ce95
Roman Gladkikh <roman.gladkikh@outlook.com>
2016-09-04 00:56:01 +0700

    IDispatcherWrapper

---
+++ VKSaver2/Core/VKSaver.Core.Services/Interfaces/IDispatcherWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace VKSaver.Core.Services.Interfaces
+{
+    public interface IDispatcherWrapper
+    {
+        CoreDispatcher Dispatcher { get; }
+    }
+}
