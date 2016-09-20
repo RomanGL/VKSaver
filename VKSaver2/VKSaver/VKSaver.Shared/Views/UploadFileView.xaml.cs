c62e4f632362d96426f54fec7f91319a0c869dc4
Roman Gladkikh <roman.gladkikh@outlook.com>
2016-09-21 00:28:21 +0700

    Добавлена страница отправки файлов.

---
+++ VKSaver2/VKSaver/VKSaver.Shared/Views/UploadFileView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Practices.Prism.StoreApps;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace VKSaver.Views
+{
+    public sealed partial class UploadFileView : VisualStateAwarePage
+    {
+        public UploadFileView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
