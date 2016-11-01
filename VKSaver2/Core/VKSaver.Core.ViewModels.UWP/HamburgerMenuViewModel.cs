e17f3a15898120008f496c8eab2e373f760f3521
Roman Gladkikh <roman.gladkikh@outlook.com>
2016-11-01 17:32:37 +0700

    HamburgerMenu на всех страницах.

---
+++ VKSaver2/Core/VKSaver.Core.ViewModels.UWP/HamburgerMenuViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Prism.Commands;
+using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKSaver.Core.ViewModels
+{
+    [ImplementPropertyChanged]
+    public sealed class HamburgerMenuViewModel : ViewModelBase
+    {
+        public HamburgerMenuViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            ItemClickCommand = new DelegateCommand<string>(OnItemClickCommand);
+        }
+
+        public int SelectedOptionsIndex { get; set; }
+
+        public int SelectedIndex { get; set; }
+
+        public bool IsPaneOpen { get; set; }
+
+        public int DisplayMode { get; set; }
+
+        [DoNotNotify]
+        public DelegateCommand<string> ItemClickCommand { get; private set; }
+
+        private void OnItemClickCommand(string tag)
+        {
+            switch (tag)
+            {
+                case "main":
+                    _navigationService.Navigate("MainView", null);
+                    break;
+                case "audios":
+                case "videos":
+                    _navigationService.Navigate("UserContentView", JsonConvert.SerializeObject(
+                        new KeyValuePair<string, string>(tag, "0")));
+                    break;
+                case "settings":
+                    _navigationService.Navigate("SettingsView", null);
+                    break;
+            }
+
+            if (DisplayMode != 3)
+                IsPaneOpen = false;
+        }
+
+        private readonly INavigationService _navigationService;
+    }
+}
---
+++ VKSaver2/VKSaver/VKSaver.UWP/Converters/HamburgerItemClickConverter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace VKSaver.Converters
+{
+    /// <summary>
+    /// Конвертирует данные события ItemClick в данные нажатого элента.
+    /// </summary>
+    public class HamburgerItemClickConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((HamburgerMenuGlyphItem)((ItemClickEventArgs)value).ClickedItem).Tag.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
