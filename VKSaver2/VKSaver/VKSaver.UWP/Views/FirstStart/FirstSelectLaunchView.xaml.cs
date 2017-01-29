ba85015bd94ad754868e13eaad44b0a19dc4c8af
Roman Gladkikh <roman.gladkikh@outlook.com>
2017-01-29 15:44:44 +0700

    Настройки в UWP.

---
+++ VKSaver2/VKSaver/VKSaver.UWP/Views/FirstStart/FirstSelectLaunchView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Windows.Mvvm;
+
+namespace VKSaver.Views
+{
+    public sealed partial class FirstSelectLaunchView : SessionStateAwarePage
+    {
+        public FirstSelectLaunchView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
