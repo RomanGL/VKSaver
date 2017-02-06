6fe382d51572275f8bc269ebf4daa4989bf11754
Roman Gladkikh <roman.gladkikh@outlook.com>
2017-02-06 16:46:44 +0700

    Двигающаяся панель.

--- VKSaver2/VKSaver/VKSaver.UWP/Controls/ChromeFrame.cs
+++ VKSaver2/VKSaver/VKSaver.UWP/Controls/ChromeFrame.cs
@@ -11,12 +11,20 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
 using VKSaver.Core.Models;
 using VKSaver.Core.Services.Interfaces;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
 
 namespace VKSaver.Controls
 {
     public sealed class ChromeFrame : Frame, IAppNotificationsPresenter
     {
         private UIElement _topBar;
+        private Grid _menuBar;
+        private UIElement _dismissLayer;
+        private Rectangle _translatePanel;
+
+        private CompositeTransform _menuBarTransform;
 
         public ChromeFrame()
         {
@@ -34,12 +42,46 @@ namespace VKSaver.Controls
             base.OnApplyTemplate();
 
             _topBar = GetTemplateChild("TopBar") as UIElement;
+            _menuBar = GetTemplateChild("MenuBar") as Grid;
+            _dismissLayer = GetTemplateChild("DismissLayer") as UIElement;
+            _translatePanel = GetTemplateChild("TranslatePanel") as Rectangle;
 
             if (!DesignMode.DesignModeEnabled)
             {
-                CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
-                Window.Current.SetTitleBar(_topBar);
+                _menuBarTransform = new CompositeTransform();
+                _menuBar.RenderTransform = _menuBarTransform;
+                _menuBarTransform.TranslateX = -_menuBar.Width;
+
+                UpdateTitleBar();
+                SubsribePaneEvents();
             }
+        }
+
+        private void UpdateTitleBar()
+        {
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            Window.Current.SetTitleBar(_topBar);
+        }
+
+        private void SubsribePaneEvents()
+        {
+            _menuBar.ManipulationDelta += _menuBar_ManipulationDelta;
+            _translatePanel.ManipulationDelta += _translatePanel_ManipulationDelta;
+        }
+
+        private void _menuBar_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            DeltaMenuBar(e);
+        }
+
+        private void _translatePanel_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            DeltaMenuBar(e);
+        }
+
+        private void DeltaMenuBar(ManipulationDeltaRoutedEventArgs e)
+        {
+            _menuBarTransform.TranslateX += e.Delta.Translation.X;
         }
     }
 }
