4b37195a7a8eaaa3c45faff01793814ffce8fdf0
Roman Gladkikh <roman.gladkikh@outlook.com>
2017-03-07 22:13:08 +0700

    Страница локальной библиотеки.

---
+++ VKSaver2/VKSaver/VKSaver.UWP/Themes/Common.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml;
+
+namespace VKSaver.Themes
+{
+    public partial class CommonDictionary : ResourceDictionary
+    {
+        public CommonDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
