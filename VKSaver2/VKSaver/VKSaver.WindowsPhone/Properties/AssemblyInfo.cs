3ce2199c7cc6a9923eb84e22e538fddff7322419
Roman Gladkikh <roman.gladkikh@outlook.com>
2017-05-20 15:29:03 +0700

    Версия 2.3.2.0

--- VKSaver2/VKSaver/VKSaver.WindowsPhone/Properties/AssemblyInfo.cs
+++ VKSaver2/VKSaver/VKSaver.WindowsPhone/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ВКачай 2")]
-[assembly: AssemblyCopyright("Roman Gladkikh ©  2016")]
+[assembly: AssemblyCopyright("Roman Gladkikh © 2017")]
 [assembly: AssemblyTrademark("ВКачай")]
 [assembly: AssemblyCulture("")]
 
@@ -25,8 +25,8 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // с помощью символа '*', как показано ниже:
 // [сборка: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.1.0")]
-[assembly: AssemblyFileVersion("2.3.1.0")]
+[assembly: AssemblyVersion("2.3.2.0")]
+[assembly: AssemblyFileVersion("2.3.2.0")]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en")]
 
