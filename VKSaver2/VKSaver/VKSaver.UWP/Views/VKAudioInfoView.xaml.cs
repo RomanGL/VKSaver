442fe67d70fbdf098b7468baa10d2ef38ed883bc
Roman Gladkikh <roman.gladkikh@outlook.com>
2017-06-01 21:50:23 +0700

    Информация о битрейте.

---
+++ VKSaver2/VKSaver/VKSaver.UWP/Views/VKAudioInfoView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Navigation;
+using Prism.Windows.Mvvm;
+
+namespace VKSaver.Views
+{
+    public sealed partial class VKAudioInfoView : SessionStateAwarePage
+    {
+        public VKAudioInfoView()
+        {
+            this.InitializeComponent();
+            this.Loaded += VKAudioInfoView_Loaded;
+        }
+
+        private void VKAudioInfoView_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.Loaded -= VKAudioInfoView_Loaded;
+            FadeInAnimation.Begin();
+        }
+    }
+}
